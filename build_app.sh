56f1584950d69e9e39225f73b2dc719b37a1b3e2
amari <amari.lawal@gmail.com>
2024-02-11 14:22:24 +0000

    amarianime is working redirect subtitle download due to CORS issues from source

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin 
-docker build -t palondomus/amarimovieswishlist:latest .
-docker push palondomus/amarimovieswishlist:latest
-docker run -it -p 8080:8080 palondomus/amarimovieswishlist:latest
+docker build -t palondomus/amarimovieswishlist:bestest .
+docker push palondomus/amarimovieswishlist:bestest
+docker run -it -p 8080:8080 palondomus/amarimovieswishlist:bestest
